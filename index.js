2bdeaca499d131ac32bb9cf495e7950967f7181d
leoniephelps <leoniephelps96@gmail.com>
2022-10-09 15:44:23 +0100

    get info from WHP

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const PORT = 8000;
+// npm run start
 const axios = require("axios");
 const cheerio = require("cheerio");
 const express = require("express");
@@ -9,11 +10,22 @@ const WhpUrl = "https://www.thewarehouseproject.com/calendar_2022";
 axios(WhpUrl).then((res) => {
   const html = res.data;
   const $ = cheerio.load(html);
-  $(".calendar_artists", html).each(function() {
-    console.log($(this).text());
+  // GET DATES
+  $(".calendar_block_date", html).each(function () {
+    //console.log($(this).text());
   });
-
-  // class="calendar_artists "
+  // GET EVENT NAMES
+  $(".calendar_name", html).each(function () {
+    //console.log($(this).text());
+  });
+  // GET ARTIST LINEUP
+  $(".calendar_artists", html).each(function () {
+    //console.log($(this).text());
+  });
+  // GET EVENT TIME AND LOCATION
+  $(".grey", html).each(function () {
+    //console.log($(this).text());
+  });
 });
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
